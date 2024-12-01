78feb13820a965fcbc7eb7057ff8862646746d7c
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-01 18:31:06 -0500

    Add day part 1 solution

---
+++ day1/calc_distance.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+from pathlib import Path
+import sys
+
+
+def main():
+    infile = Path(sys.argv[1])
+    with infile.open("r") as f:
+        nums: list[tuple[int, int]] = [
+            (int(a), int(b)) for a, b in (line.split() for line in list(f))
+        ]
+    col0, col1 = zip(*nums)
+    dist_map = map(
+        lambda a, b: abs(a - b),
+        sorted(col0),
+        sorted(col1),
+    )
+    print(sum(dist_map))
+
+
+if __name__ == "__main__":
+    main()
