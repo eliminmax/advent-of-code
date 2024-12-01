f2eef32f842f645410e31bfabd567656e3328585
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-01 18:51:03 -0500

    Day 1

---
+++ dec01/calc_similarity.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+from pathlib import Path
+import sys
+from functools import cache
+
+
+# cache lookups
+@cache
+def get_frequency(n: int, col: list[int]) -> int:
+    return n * col.count(n)
+
+
+def main():
+    infile = Path(sys.argv[1])
+    with infile.open("r") as f:
+        nums: list[tuple[int, int]] = [
+            (int(a), int(b)) for a, b in (line.split() for line in list(f))
+        ]
+    col0, col1 = zip(*nums)
+    similarity_map = map(lambda n: get_frequency(n, col1), col0)
+    print(sum(similarity_map))
+
+
+if __name__ == "__main__":
+    main()
