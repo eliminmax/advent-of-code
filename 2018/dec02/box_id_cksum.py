0a0a8fe49f29bdc1a4c3902a782f226ae59a6eb7
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-02 17:02:31 -0500

    2018 day 2

---
+++ 2018/dec02/box_id_cksum.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2018 Day 2 Part 1
+
+import sys
+import string
+
+
+def has_n(box_id: str, count: int) -> bool:
+    for c in string.ascii_lowercase:
+        if box_id.count(c) == count:
+            return True
+    return False
+
+
+def main():
+    total_with_2 = 0
+    total_with_3 = 0
+    with open(sys.argv[1], "r") as f:
+        for line in f:
+            if has_n(line, 2):
+                total_with_2 += 1
+            if has_n(line, 3):
+                total_with_3 += 1
+    print(total_with_2 * total_with_3)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2018/dec02/find_similar.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2018 Day 2 Part 2
+
+import sys
+
+
+def find_similar(box_id: str, known_ids: list[str]) -> None | str:
+    for known_id in known_ids:
+        for i in range(len(known_id)):
+            if box_id[:i] + box_id[i+1:] == known_id[:i] + known_id[i+1:]:
+                return box_id[:i] + box_id[i+1:]
+
+
+def main():
+    with open(sys.argv[1], "r") as f:
+        lines = [line.strip() for line in f]
+    for i in range(len(lines)):
+        result = find_similar(lines[i], lines[:i] + lines[i+1:])
+        if result is not None:
+            print(result)
+            break
+
+
+if __name__ == "__main__":
+    main()
