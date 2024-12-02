5f5faec1ca34c2cef7ca309b8c2be14370cb2a92
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-02 15:54:45 -0500

    2017 day 1

---
+++ 2017/dec01/anticaptcha_next_digit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2017 Day 1 Part 1
+
+import sys
+
+
+def main():
+    with open(sys.argv[1], "r") as f:
+        digits = f.read()
+    count = 0
+    for digit, next_digit in zip(digits, digits[1:] + digits[0]):
+        if digit == next_digit:
+            count += int(digit)
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
