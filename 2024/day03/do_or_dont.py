a29385b4edab6835f70e3002e5b95ebc289ad4f8
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-03 12:42:26 -0500

    2024 day 3

---
+++ 2024/day03/do_or_dont.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2024 Day 3 Part 2
+
+import sys
+import re
+
+mul_pat = "(?:mul\\((?P<arg0>[0-9]+),(?P<arg1>[0-9]+)\\))"
+enable_pat = "(?P<enable>do\\(\\))"
+disable_pat = "(?P<disable>don't\\(\\))"
+pat = re.compile("|".join((mul_pat, enable_pat, disable_pat)))
+
+
+def main():
+    total = 0
+    enabled = True
+    with open(sys.argv[1], "r") as f:
+        for match in pat.finditer(f.read()):
+            groupdict = match.groupdict()
+            if groupdict["enable"]:
+                enabled = True
+            elif groupdict["disable"]:
+                enabled = False
+            elif enabled:
+                total += (int(groupdict["arg0"]) * int(groupdict["arg1"]))
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/day03/filter_corrupt.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2024 Day 3 Part 1
+
+import sys
+import re
+
+mul_pat = re.compile("mul\\(([0-9]+),([0-9]+)\\)")
+
+
+def main():
+    total = 0
+    with open(sys.argv[1], "r") as f:
+        for match in mul_pat.finditer(f.read()):
+            group = match.groups()
+            total += (int(group[0]) * int(group[1]))
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
