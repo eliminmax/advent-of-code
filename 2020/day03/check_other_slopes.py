28e60d2c02a9b63b50af84780daf14748839358d
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-04 15:03:50 -0500

    2020 day 3

---
+++ 2020/day03/check_other_slopes.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2020 Day 3 Part 2
+
+import sys
+from itertools import cycle
+from collections.abc import Iterable, Generator, Sequence
+from typing import TypeVar
+
+T = TypeVar('T')
+
+
+def column_generator(template: Sequence[Iterable[T]]) -> Generator[list[T]]:
+    cycles = [cycle(i) for i in template]
+    while True:
+        yield [next(i) for i in cycles]
+
+
+def slope_check(rows: Sequence[Iterable[str]], right: int, down: int) -> int:
+    cols = column_generator(rows)
+    # skip starting column
+    _ = next(cols)
+    total = 0
+    for i in range(down, len(rows), down):
+        result = "."
+        # 3 over, 1 down
+        for _ in range(right):
+            result = next(cols)
+        if result[i] == "#":
+            total += 1
+    return total
+
+
+def main():
+    with open(sys.argv[1], "r") as f:
+        rows: list[Iterable[str]] = [line.strip() for line in f]
+    product = 1
+    for right, down in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)):
+        product *= slope_check(rows, right, down)
+    print(product)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2020/day03/tree_hits.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2020 Day 3 Part 1
+
+import sys
+from itertools import cycle
+from collections.abc import Iterable, Generator, Sequence
+from typing import TypeVar
+
+T = TypeVar('T')
+
+
+def column_generator(template: Sequence[Iterable[T]]) -> Generator[list[T]]:
+    cycles = [cycle(i) for i in template]
+    while True:
+        yield [next(i) for i in cycles]
+
+
+def main():
+    with open(sys.argv[1], "r") as f:
+        rows: list[Iterable[str]] = [line.strip() for line in f]
+    cols = column_generator(rows)
+    # move over 3 and down 1
+    total = 0
+    # skip starting column
+    _ = next(cols)
+    for i in range(1, len(rows)):
+        # 3 over, 1 down
+        _ = next(cols)
+        _ = next(cols)
+        if next(cols)[i] == "#":
+            total += 1
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
