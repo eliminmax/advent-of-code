cf800d82325905ae0a15bc879e711747b873827f
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-05 15:37:27 -0500

    2017 day 4

---
+++ 2017/day04/part2_2017_04.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2017 Day 4 Part 2
+
+import sys
+
+
+def main() -> None:
+    counter = 0
+    with open(sys.argv[1], "r") as f:
+        lines = list(f)
+    for line in lines:
+        words = ["".join(sorted(word)) for word in line.split()]
+        if sorted(set(words)) == sorted(words):
+            counter += 1
+    print(counter)
+
+
+if __name__ == "__main__":
+    main()
