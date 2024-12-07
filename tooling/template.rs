890f433e575b343ed4776dc262a8be96b7b145a7
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-07 18:17:04 -0500

    Fix bug in rust template

--- tooling/template.rs
+++ tooling/template.rs
@@ -6,5 +6,6 @@ use std::fs::read;
 use std::fs::read_to_string;
 
 fn main() {
-    let mut input = read_to_string(args().nth(1).unwrap_or("input")).expect("Failed to read file!");
+    let mut input = read_to_string(args().nth(1).unwrap_or(String::from("input")))
+        .expect("Failed to read file!");
 }
