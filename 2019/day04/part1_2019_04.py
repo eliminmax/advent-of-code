3b37f2b05135e406b3a99353f4b5094b19196c1a
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-07 19:33:23 -0500

    2019 day 4

---
+++ 2019/day04/part1_2019_04.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2019 Day 4 Part 1
+
+import sys
+import re
+from itertools import pairwise
+repeat_pat = re.compile(r"(\d)\1")
+
+
+def main() -> None:
+    with open(sys.argv[1] if sys.argv[1:] else "input", "r") as f:
+        range_min, range_max = (int(i) for i in f.read().strip().split("-"))
+
+    counter = 0
+    for n in range(range_min, range_max + 1):
+        n_str: str = str(n)
+        if all(map(lambda s: int(s[0]) <= int(s[1]), pairwise(n_str))):
+            if repeat_pat.search(n_str):
+                counter += 1
+    print(counter)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2019/day04/part2_2019_04.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2019 Day 4 Part 2
+
+import sys
+from itertools import pairwise
+
+
+def has_pair(s: str) -> bool:
+    pairs: list[str] = list(
+        "".join((a, b)) for a, b in pairwise(s)
+    )
+    for i, pair in enumerate(pairs):
+        if pairs[i + 1 :] and pair == pairs[i + 1]:
+            continue
+        if i and pair == pairs[i - 1]:
+            continue
+        a = pair[0]
+        b = pair[1]
+        if a == b:
+            return True
+    return False
+
+
+def main() -> None:
+    with open(sys.argv[1] if sys.argv[1:] else "input", "r") as f:
+        range_min, range_max = (int(i) for i in f.read().strip().split("-"))
+
+    counter = 0
+    for n in range(range_min, range_max + 1):
+        n_str: str = str(n)
+        if all(map(lambda s: int(s[0]) <= int(s[1]), pairwise(n_str))):
+            if has_pair(n_str):
+                counter += 1
+    print(counter)
+
+
+if __name__ == "__main__":
+    main()
