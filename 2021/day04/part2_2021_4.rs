233eca2f183ca815fc2d84b468c389d5906a3669
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-07 22:37:43 -0500

    2021 day 4

---
+++ 2021/day04/part2_2021_4.rs
@@ -0,0 +1,131 @@
+// SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+//
+// SPDX-License-Identifier: 0BSD
+
+// Solution to AoC 2021 Day 4 Part 2
+
+use std::convert::TryInto;
+use std::env::args;
+use std::fs::read_to_string;
+use std::str::FromStr;
+
+#[derive(Debug, Copy, Clone)]
+struct Space {
+    space_num: u8,
+    occupied: bool,
+}
+
+impl From<u8> for Space {
+    fn from(n: u8) -> Self {
+        Space {
+            space_num: n,
+            occupied: false,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct BingoBoard([[Space; 5]; 5]);
+
+impl BingoBoard {
+    fn score(&self) -> u32 {
+        self.0
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .filter_map(|c| {
+                        if c.occupied {
+                            None
+                        } else {
+                            Some(c.space_num as u32)
+                        }
+                    })
+                    .sum::<u32>()
+            })
+            .sum()
+    }
+    fn check_victory(&self, row: usize, col: usize) -> Option<u32> {
+        if self.0[row].iter().all(|c| c.occupied) || self.0.iter().all(|r| r[col].occupied) {
+            Some(self.score())
+        } else {
+            None
+        }
+    }
+
+    fn mark(&mut self, n: u8) -> Option<u32> {
+        for (row_num, row) in self.0.iter_mut().enumerate() {
+            for (col, cell) in row.iter_mut().enumerate() {
+                if cell.space_num == n {
+                    cell.occupied = true;
+                    return self.check_victory(row_num, col);
+                }
+            }
+        }
+        None
+    }
+}
+
+impl FromStr for BingoBoard {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let cells: Result<Vec<u8>, Self::Err> = s
+            .split_whitespace()
+            .map(|c| u8::from_str(c).map_err(|_| "failed to parse u8"))
+            .collect();
+        let cells: Vec<Space> = cells?.into_iter().map(Space::from).collect();
+        match cells.len() {
+            i if i > 25 => Err("Too many cells!"),
+            i if i < 25 => Err("Too few cells!"),
+            _ => {
+                let mut cell_slices = cells.chunks_exact(5);
+                macro_rules! next_row {
+                    () => {{
+                        let row: [Space; 5] = cell_slices
+                            .next()
+                            .expect("Failed to get row")
+                            .try_into()
+                            .expect("Wrong number of elements");
+                        row
+                    }};
+                }
+                Ok(BingoBoard([
+                    next_row!(),
+                    next_row!(),
+                    next_row!(),
+                    next_row!(),
+                    next_row!(),
+                ]))
+            }
+        }
+    }
+}
+
+fn main() {
+    let input = read_to_string(args().nth(1).unwrap_or(String::from("input")))
+        .expect("Failed to read file!");
+    let mut line_groups = input.split("\n\n");
+    let mut nums = line_groups
+        .next()
+        .expect("Failed to read first line")
+        .split(',')
+        .map(|c| u8::from_str(c.trim()).expect("Failed to parse number"));
+    let mut boards: Vec<BingoBoard> = line_groups
+        .map(|b| BingoBoard::from_str(b).expect("Failed to parse board"))
+        .collect();
+    'bingo: for num in nums.by_ref() {
+        boards.retain_mut(|board| board.mark(num).is_none());
+        if boards.len() <= 1 {
+            break 'bingo;
+        }
+    }
+    if let Some(mut board) = boards.pop() {
+        'remaining: for num in nums {
+            if let Some(score) = board.mark(num) {
+                println!("{}", score * num as u32);
+                break 'remaining;
+            }
+        }
+    } else {
+        panic!("No single board left over!");
+    }
+}
