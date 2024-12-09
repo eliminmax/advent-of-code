53b2048b29626170ab38e3e12e8b5aabfb72153a
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-09 16:57:34 -0500

    Add docstring & type hints to advent_debug.eprint

--- tooling/pylib/advent_debug.py
+++ tooling/pylib/advent_debug.py
@@ -3,13 +3,17 @@
 # SPDX-License-Identifier: 0BSD
 
 from sys import stderr
+from typing import Any
 
 from rich import print as rprint
 
 
-def eprint(*args, **kwargs):
+def eprint(*args: Any, **kwargs: Any):
     """Wrapper around rich.print that defaults to file=sys.stderr
-    Has the same semantics as print."""
+    Has the same semantics as rich.print (i.e. the same as print, but with
+    support for rich console markup.
+
+    https://rich.readthedocs.io/en/stable/markup.html#console-markup"""
     if "file" not in kwargs:
         kwargs["file"] = stderr
     rprint(*args, **kwargs)
