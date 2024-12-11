c5b3d920280ba16322dfe6d3f282dd74f258e4a4
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-11 12:58:40 -0500

    2016 day 6
    Part 2 was just using tooling/gen-part2.sh, then running
    `sed s/ax/in/ -i part2_2016_6.py`.

---
+++ 2016/day06/part1_2016_6.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2016 Day 6 Part 1
+
+import sys
+from collections import defaultdict
+from typing import TypeAlias
+
+
+CharCount: TypeAlias = defaultdict[str, int]
+
+
+def main() -> None:
+    template: CharCount = defaultdict(int)
+    position_counts: defaultdict[int, CharCount] = defaultdict(template.copy)
+    with open(sys.argv[1] if sys.argv[1:] else "input", "r") as f:
+        for line in f:
+            word = line.strip()
+            for i, c in enumerate(word):
+                position_counts[i][c] += 1
+    most_common: list[str] = [""] * len(position_counts)
+    for pos, counts in position_counts.items():
+        most_common[pos] = max(counts, key=lambda c: counts[c])
+    print("".join(most_common))
+
+
+if __name__ == "__main__":
+    main()
