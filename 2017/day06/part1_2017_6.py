93de9a024799a7d27a9d9dd9cae268ce7c16f4fd
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-11 12:13:46 -0500

    2017 day 6
    I clicked the wrong page without noticing, and thought I was on 2016 day
    6.

---
+++ 2017/day06/part1_2017_6.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2017 Day 6 Part 1
+
+import sys
+
+
+def main() -> None:
+    with open(sys.argv[1] if sys.argv[1:] else "input", "r") as f:
+        blocks = [int(i) for i in f.read().split()]
+    seen_states: list[list[int]] = []
+    redists = 0
+    redist_sz = len(blocks)
+    while blocks not in seen_states:
+        seen_states.append(blocks[:])
+        redist_amnt = max(blocks)
+        index = blocks.index(redist_amnt)
+        blocks[index] = 0
+        while redist_amnt:
+            index += 1
+            index %= redist_sz
+            blocks[index] += 1
+            redist_amnt -= 1
+        redists += 1
+    print(redists)
+
+
+if __name__ == "__main__":
+    main()
