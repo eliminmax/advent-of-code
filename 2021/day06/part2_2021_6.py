9db05ab0ee7e7b2157287aeb83bd8f2ca6acb0da
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-11 15:50:58 -0500

    2021 day 6
    Done on 2024 day 11, which is quite similar, but was less cachable than
    this is. Caching makes this one trivial.

---
+++ 2021/day06/part2_2021_6.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+#
+# SPDX-License-Identifier: 0BSD
+
+# Solution to AoC 2021 Day 6 Part 2
+
+import sys
+from functools import cache
+
+
+@cache
+def lanternfish_count(cycle: int, remaining_days: int = 256) -> int:
+    if remaining_days == 0:
+        return 1
+    if cycle == 0:
+        newborn = lanternfish_count(8, remaining_days - 1)
+        thisone = lanternfish_count(6, remaining_days - 1)
+        return newborn + thisone
+    return lanternfish_count(cycle - 1, remaining_days - 1)
+
+
+def main() -> None:
+    with open(sys.argv[1] if sys.argv[1:] else "input", "r") as f:
+        fish: list[int] = [int(i) for i in f.read().strip().split(",")]
+    print(sum(lanternfish_count(i) for i in fish))
+
+
+if __name__ == "__main__":
+    main()
