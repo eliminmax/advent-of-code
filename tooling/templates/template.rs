c75b9cf9bbc1d3064f599efa7aafee6477b0345f
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-25 16:45:54 -0500

    Update Rust template

--- tooling/templates/template.rs
+++ tooling/templates/template.rs
@@ -1,11 +1,9 @@
 // --solution-comment--
 
 use std::env::args;
-// Choose one of the following:
-use std::fs::read;
 use std::fs::read_to_string;
 
 fn main() {
-    let mut input = read_to_string(args().nth(1).unwrap_or(String::from("input")))
+    let input = read_to_string(args().nth(1).unwrap_or(String::from("input")))
         .expect("Failed to read file!");
 }
