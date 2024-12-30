e81e2b9c22d9b205ba91250feda1ba4cf7e601a9
Eli Array Minkoff <eli@planetminkoff.com>
2024-12-30 13:45:17 -0500

    Move input-loading `uses` into main

--- tooling/templates/template.rs
+++ tooling/templates/template.rs
@@ -1,9 +1,8 @@
 // --solution-comment--
 
-use std::env::args;
-use std::fs::read_to_string;
-
 fn main() {
+    use std::env::args;
+    use std::fs::read_to_string;
     let input = read_to_string(args().nth(1).unwrap_or(String::from("input")))
         .expect("Failed to read file!");
 }
