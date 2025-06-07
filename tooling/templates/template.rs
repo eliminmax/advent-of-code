d8d6784658494ebe5794541af30f7697e7f921fd
Eli Array Minkoff <eli@planetminkoff.com>
2025-06-07 17:09:20 -0400

    Update rust template to use as_deref

--- tooling/templates/template.rs
+++ tooling/templates/template.rs
@@ -3,6 +3,6 @@
 fn main() {
     use std::env::args;
     use std::fs::read_to_string;
-    let input = read_to_string(args().nth(1).unwrap_or(String::from("input")))
-        .expect("Failed to read file!");
+    let input =
+        read_to_string(args().nth(1).as_deref().unwrap_or("input")).expect("Failed to read file!");
 }
