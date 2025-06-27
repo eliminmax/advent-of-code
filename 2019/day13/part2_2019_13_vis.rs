35e7df6a584c1c37fd9a533276d7e64ef22e9cab
Eli Array Minkoff <eli@planetminkoff.com>
2025-06-27 16:04:44 -0400

    Rework 2019 day 13 part 2 visualizer
    Now uses non-ASCII characters for display, and displays score on the
    top.
    
    It no longer uses a HashMap for the screen, instead executing draw
    instructions directly as needed, and storing the x positions of
    the ball and paddle only.
    
    It also moves the cursor to the end of the screen at the end of execution.

--- 2019/day13/part2_2019_13_vis.rs
+++ 2019/day13/part2_2019_13_vis.rs
@@ -34,54 +34,32 @@ impl TryFrom<i64> for Tile {
 
 type Screen = HashMap<(i64, i64), Tile>;
 
-fn select_input(screen: &Screen) -> i64 {
-    let mut ball_x: Option<i64> = None;
-    let mut paddle_x: Option<i64> = None;
-    for (&(x, _), &tile) in screen.iter() {
-        match tile {
-            Tile::Ball => {
-                assert!(ball_x.is_none());
-                ball_x = Some(x);
+impl std::fmt::Display for Tile {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            fmt,
+            "{}",
+            match self {
+                Tile::Empty => " ",
+                Tile::HorizPaddle => "\x1b[38;5;226m─\x1b[39m", // bright yellow line
+                Tile::Block => "\x1b[38;5;68m❒\x1b[39m",        // cornflower blue box
+                Tile::Ball => "\x1b[38;5;196m●\x1b[39m",        // red circle
+                Tile::Wall => "\x1b[38;5;28m█\x1b[39m",         // green block
             }
-            Tile::HorizPaddle => {
-                assert!(paddle_x.is_none());
-                paddle_x = Some(x);
-            }
-            _ => (),
-        }
+        )
     }
-    ball_x.cmp(&paddle_x) as i64
 }
-fn render(screen: &Screen, score: i64) {
-    use std::{thread, time::Duration};
-    let (mut x_min, mut y_min, mut x_max, mut y_max) = (0, 0, 0, 0);
-    for &(x, y) in screen.keys() {
-        x_min = x_min.min(x);
-        x_max = x_max.max(x);
-        y_min = y_min.min(y);
-        y_max = y_max.max(y);
-    }
-    print!("\x1b[H\x1b[1m"); // move to top corner and bold input
 
-    for y in y_min..=y_max {
-        for x in x_min..=x_max {
-            match screen.get(&(x, y)).cloned().unwrap_or_default() {
-                Tile::Empty => print!(" "),
-                Tile::HorizPaddle => print!("\x1b[38;5;226m-"), // bright yellow dash
-                Tile::Block => print!("\x1b[38;5;68m#"),        // cornflower blue pound sign
-                Tile::Ball => print!("\x1b[38;5;196mo"),        // red lowercase o
-                Tile::Wall => print!("\x1b[48;5;28m \x1b[48;5;16m"), // space with green background
-            }
-        }
-        println!();
-    }
-    println!("\x1b[38;5;231mScore: {score}\x1b[m");
-    thread::sleep(Duration::from_millis(15));
+fn render(x: i64, y: i64, tile: Tile) {
+    let x = u8::try_from(x).unwrap() + 1;
+    let y = u8::try_from(y).unwrap() + 2;
+    print!("\x1b[{y};{x}H{tile}");
 }
 
 fn main() {
     use std::env::args;
     use std::fs::read_to_string;
+    use std::io::{stdout, Write};
     let input =
         read_to_string(args().nth(1).as_deref().unwrap_or("input")).expect("Failed to read file!");
     let mut interpreter =
@@ -89,27 +67,37 @@ fn main() {
     interpreter.mem_override(0, 2);
 
     let mut screen = Screen::new();
-    let mut score = 0;
+    let mut ball_x: i64 = 0;
+    let mut paddle_x: i64 = 0;
 
     let mut inputs = Vec::new();
-    print!("\x1b[H\x1b[J\x1b[3J");
+    print!("\x1b[?25l\x1b[H\x1b[J\x1b[3J");
+    stdout().lock().flush().unwrap();
     loop {
-        let (draw_instructions, _) = interpreter.run_through_inputs(inputs.drain(..)).unwrap();
+        let (draw_instructions, outcome) =
+            interpreter.run_through_inputs(inputs.drain(..)).unwrap();
         for trio in draw_instructions.chunks(3) {
             match (trio[0], trio[1]) {
-                (-1, 0) => score = trio[2],
-                pos => {
-                    let _ = screen.insert(pos, Tile::try_from(trio[2]).unwrap());
+                (-1, 0) => print!("\x1b[H\x1b[2K\x1b[1;7mScore: {}\x1b[22;27m", trio[2]),
+                (x, y) => {
+                    let tile = Tile::try_from(trio[2]).unwrap();
+                    if tile == Tile::Ball {
+                        ball_x = x;
+                    } else if tile == Tile::HorizPaddle {
+                        paddle_x = x;
+                    }
+                    screen.insert((x, y), tile);
+                    render(x, y, tile);
                 }
             }
         }
-        render(&screen, score);
-        inputs.push(select_input(&screen));
-
-        if screen.values().all(|t| *t != Tile::Block) {
+        stdout().lock().flush().unwrap();
+        inputs.push(ball_x.cmp(&paddle_x) as i64);
+        std::thread::sleep(std::time::Duration::from_millis(15));
+        if outcome == intcode::State::Halted {
             break;
         }
     }
-
-    render(&screen, score);
+    let y_max = screen.keys().map(|&(_, y)| y + 2).max().unwrap_or(1);
+    println!("\x1b[{y_max}H\x1b[?25h");
 }
