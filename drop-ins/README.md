d8151a1a2235e6109b042c694d4ffd59309679ae
Eli Array Minkoff <eli@planetminkoff.com>
2025-07-29 20:55:42 -0400

    Add drop-ins directory for repeatedly-used code
    `intcode.rs` is retroactively added. It's the most complete iteration,
    from 2019 day 25.
    
    `dijkstra.rs` is a newly-written generic implementation of dijkstra's
    algorithm, which can be copied into the day's directory and included as
    a module.

---
+++ drop-ins/README.md
@@ -0,0 +1,3 @@
+# Drop-ins
+
+Directory for algorithms and/or data structures that frequently appear within different Advent of Code solutions, to add to as needed.
---
+++ drop-ins/rust/dijkstra.rs
@@ -0,0 +1,45 @@
+// SPDX-FileCopyrightText: 2025 Eli Array Minkoff
+//
+// SPDX-License-Identifier: 0BSD
+
+//! Module containing a generic implementation of Dijkstra's Algorithm, which returns a HashMap
+//! that maps nodes to the costs to reach them.
+
+use core::cmp::Reverse;
+use core::hash::Hash;
+use core::ops::Add;
+use std::collections::{BinaryHeap, HashMap};
+
+/// A generic implementation of Dijkstra's Algorithm
+/// `start` is the node to start from, and `neighbor_fn` returns a IntoIterator of neighbors and
+/// the costs to go to them from an the location
+pub fn dijkstra<Node, NeighborIter, Cost, NeighborFn>(
+    start: Node,
+    neighbor_fn: NeighborFn,
+) -> HashMap<Node, Cost>
+where
+    Node: Hash + Ord + Copy + Eq + std::fmt::Debug,
+    Cost: Ord + Copy + Eq + std::fmt::Debug + Default + Add<Cost, Output = Cost>,
+    NeighborIter: IntoIterator<Item = (Node, Cost)>,
+    NeighborFn: Fn(Node) -> NeighborIter,
+{
+    let mut costs: HashMap<Node, Cost> = HashMap::from([(start, Cost::default())]);
+    let mut queue: BinaryHeap<Reverse<(Cost, Node)>> =
+        BinaryHeap::from([Reverse((Cost::default(), start))]);
+
+    while let Some(Reverse((cost, node))) = queue.pop() {
+        if costs[&node] < cost {
+            continue;
+        }
+
+        for (neighbor, conn_cost) in neighbor_fn(node) {
+            let next_cost = cost + conn_cost;
+            if costs.get(&neighbor).is_none_or(|prev| *prev > next_cost) {
+                costs.insert(neighbor, next_cost);
+                queue.push(Reverse((next_cost, neighbor)));
+            }
+        }
+    }
+
+    costs
+}
